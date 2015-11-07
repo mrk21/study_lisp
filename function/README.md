9669631a1f41a9fb171c07f74062135999743a2a
Yuichi Murata <mrk21info+github@gmail.com>
2015-11-07 21:37:01 +0900

    function: append a code of lambda

--- function/README.md
+++ function/README.md
@@ -2,4 +2,5 @@
 
 ## See
 
-* [Common Lisp 入門](http://www.geocities.jp/m_hiroi/xyzzy_lisp/abclisp02.html#chap04)
+* [関数定義 - Common Lisp 入門](http://www.geocities.jp/m_hiroi/xyzzy_lisp/abclisp02.html#chap04)
+* [高階関数とラムダ式 - Common Lisp 入門](http://www.geocities.jp/m_hiroi/xyzzy_lisp/abclisp05.html)
--- function/main.lisp
+++ function/main.lisp
@@ -1,2 +1,27 @@
+(format t "## function")
 (defun square (x) (* x x))
-(prin1 (square 2))
+(print (square 2))
+(format t "~2%")
+
+
+(format t "## lambda")
+(print ((lambda (x) (* x x)) 2))
+(format t "~2%")
+
+
+; map
+(format t "## mapcar")
+(print (mapcar #'(lambda (x) (* x x)) '(1 2 3 4 5)))
+(format t "~2%")
+
+
+; JavaScript: apply
+(format t "## apply")
+(print (apply #'+ '(1 2 3 4 5)))
+(format t "~2%")
+
+
+; JavaScript: call
+(format t "## funcall")
+(print (funcall #'+ 1 2 3 4 5))
+(format t "~2%")
