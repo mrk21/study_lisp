ec7e3a2779e52d28e1836236948e3c021bc74812
Yuichi Murata <mrk21info+github@gmail.com>
2016-01-11 17:17:04 +0900

    fix a code of quick sort

--- other/main.lisp
+++ other/main.lisp
@@ -1,19 +1,23 @@
-(defun filter (list pivot f)
+; Quick sort
+(defun partial (condition list pivot)
   (cond
     ((null list)
       nil)
-    ((funcall f (car list) pivot)
-      (cons (car list) (filter (cdr list) pivot f)))
+    ((funcall condition (car list) pivot)
+      (cons (car list) (partial condition (cdr list) pivot)))
     (t
-      (filter (cdr list) pivot f))))
+      (partial condition (cdr list) pivot))))
+
+(defun pivot (list) (car list))
+(defun without_pivot (list) (cdr list))
 
 (defun qsort (list)
   (cond
     ((null list) nil)
     (t
       (append
-        (qsort (filter (cdr list) (car list) #'<))
-        (cons (car list) nil)
-        (qsort (filter (cdr list) (car list) #'>=))))))
+        (qsort (partial #'< (without_pivot list) (pivot list)))
+        (cons (pivot list) nil)
+        (qsort (partial #'>= (without_pivot list) (pivot list)))))))
 
-(print (qsort '(33 1 23 45 -12 99 102 -3)))
+(print (qsort '(33 1 23 33 45 -12 99 102 -3)))
